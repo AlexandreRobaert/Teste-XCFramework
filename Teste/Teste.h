7a6baf745384d7a0c7f5d77a5c75e33f574a6f58
Alexandre Robaert <robaert.developer.com>
2024-08-09 11:45:55 -0300

    Initial Commit

---
+++ Teste/Teste.h
@@ -0,0 +1,18 @@
+//
+//  Teste.h
+//  Teste
+//
+//  Created by Alexandre Robaert on 09/08/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Teste.
+FOUNDATION_EXPORT double TesteVersionNumber;
+
+//! Project version string for Teste.
+FOUNDATION_EXPORT const unsigned char TesteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Teste/PublicHeader.h>
+
+
